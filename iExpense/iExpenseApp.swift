ae52bfced0e09962a0b0d672d3eebe3899461317
MaratXcode <kosjachok8@yandex.ru>
2024-09-27 19:55:27 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Marat Fakhrizhanov on 27.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
